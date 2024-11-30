16118413303c7aee359424d3be9a882f32d4fa46
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-11-30 14:50:59 +0530

    hr:

--- front-end/src/components/Hero.tsx
+++ front-end/src/components/Hero.tsx
@@ -1,20 +1,32 @@
-import { FaXTwitter } from "react-icons/fa6"
+    import { FaXTwitter, FaYoutube } from "react-icons/fa6"
 import { MdDeleteOutline } from "react-icons/md"
-
 export default function Hero(){
-    const content = {}
+    const content = [
+        {link: "https://x.com/DThompsonDev/status/1862369067165712697",type:"twitter",title:"Job"},
+        {link: "https://www.youtube.com/watch?v=1gvGn8NtIpE",type:"youtube",title:"Cyberpunk"},
+    ]
+
+
 
     return <div className="container">
-        <div className="">
-            <div className="w-[400px] h-[450px] p-4 rounded-xl bg-slate-300 shadow shadow-lg">
-                <div className="flex  justify-between items-center">
-                    <h1 className="text-xl inline-flex  justify-center  items-center gap-2 font-semibold"><FaXTwitter/>Tweets</h1>
-                        <span className="text-white hover:cursor-pointer hover:scale-105 text-2xl" ><MdDeleteOutline /></span>
+         <blockquote className="twitter-tweet text-black">
+                     <a href="https://x.com/fromAlqama/status/1862510295882641507"></a> 
+                </blockquote>
+        <div className="flex  gap-8 ">
+           {content.map((item,index)=> <div key={index} className="w-[400px] h-[450px] p-4 rounded-xl bg-slate-100  shadow-lg">
+                <div className="flex  justify-between border-b-2  pb-2 items-center">
+                    <h1 className="text-xl inline-flex  justify-center  items-center gap-2 font-semibold">{item.type=="twitter"? <FaXTwitter/>: <FaYoutube className="text-2xl"/>}{item.type=="twitter"? "Tweets": "Youtube"}</h1>
+                        <span className="text-black hover:cursor-pointer hover:scale-110 text-2xl" ><MdDeleteOutline /></span>
                 </div>
-                <div className="imed">
-                    
+                <h1 className="text-xl font-semibold text-center mt-4 underline capitalize tracking-wider leading-none ">{item.title}</h1>
+                <div className="imbed mt-8  w-full   rounded-xl">
+                {item.type =="twitter"? <div><blockquote className="twitter-tweet">
+        <a href="https://twitter.com/username/status/807811447862468608"></a> 
+      </blockquote></div>:<iframe className="rounded-xl ml-2" width={"350px"} height={"300px"} src={item.link.replace("watch?v=", "embed/")} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
                 </div>
-            </div>
+            </div>)}
         </div>
     </div>
 }
+
+// rc="https://www.youtube.com/embed/QaBBgNjc5Pg?si=k59FgvgScK1TabZZ"
