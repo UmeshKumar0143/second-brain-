55225f087959549efeefac636cd05188098fc320
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-12-01 14:02:18 +0530

    user chack on fe

--- front-end/src/components/Hero.tsx
+++ front-end/src/components/Hero.tsx
@@ -1,4 +1,6 @@
-    import { FaXTwitter, FaYoutube } from "react-icons/fa6"
+    import { useState } from "react"
+import { DiVim } from "react-icons/di"
+import { FaXTwitter, FaYoutube } from "react-icons/fa6"
 import { MdDeleteOutline } from "react-icons/md"
 import { Link } from "react-router-dom"
 export default function Hero(){
@@ -6,7 +8,7 @@ export default function Hero(){
         {link: "https://twitter.com/three_cube/status/1847014253511037322",type:"twitter",title:"Job"},
         {link: "https://www.youtube.com/watch?v=1gvGn8NtIpE",type:"youtube",title:"Cyberpunk"},
     ]
-
+    const [isUser , setisUser ] = useState(""); 
     
 
 
@@ -14,7 +16,7 @@ export default function Hero(){
          <blockquote className="twitter-tweet text-black">
                      <a href="https://x.com/fromAlqama/status/1862510295882641507"></a> 
                 </blockquote>
-        <div className="flex  gap-8 ">
+        {isUser? <div className="flex  gap-8 ">
            {content.map((item,index)=> <div key={index} id="cards" className=" w-[400px] overflow-hidden overflow-y-auto h-[450px] p-4 rounded-xl bg-slate-100  shadow-lg">
                 <div className="flex  justify-between border-b-2  pb-2 items-center">
                     <h1 className="text-xl inline-flex  justify-center  items-center gap-2 font-semibold">{item.type=="twitter"? <FaXTwitter/>: <FaYoutube className="text-2xl"/>}{item.type=="twitter"? "Tweets": "Youtube"}</h1>
@@ -27,7 +29,7 @@ export default function Hero(){
       </blockquote></div>:<iframe className="rounded-xl ml-2" width={"350px"} height={"300px"} src={item.link.replace("watch?v=", "embed/")} title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
                 </div>
             </div>)}
-        </div>
+        </div>: <div className="w-full h-screen flex justify-center items-center "><div><h1 className="text-5xl font-bold capitalize ">Log in First to see content</h1></div></div>}
       
     </div>
 }
--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -2,21 +2,33 @@ import { CiShare2 } from "react-icons/ci";
 import { FaPlus } from "react-icons/fa6";
 import AddContent from "./AddContent";
 import { useState } from "react";
+import { IoMdExit } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar(){
     const  [isOpen, setIsOpen] = useState(false); 
+    const [isUser, setisUser] = useState("umesh"); 
+    const navigate = useNavigate(); 
     const HandleOpen =()=>{
         setIsOpen(true)
+    }
+    const HandleLogout = ( )=>{
+        setisUser(""); 
     }
     return <div className="container ">
         <div className=" sm:flex justify-between sm:flex-row flex-col  p-5 "> 
             <div className="">
             <h1 className="sm:text-3xl text-center text-xl font-bold uppercase tracking-wide   ">All Notes </h1>
             </div>
-            <div className="flex justify-center items-center mt-3 sm:mt-0 gap-5">
-                <button className="sm:text-xl  text-base font-semibold px-2 py-2 sm:px-4 sm:py-2 bg-purple-300 flex items-center justify-center gap-2 rounded-xl text-purple-500" ><CiShare2/>Share Brain</button>
+            {isUser? <div className="flex justify-center items-center mt-3 sm:mt-0 gap-5">
                 <button onClick={HandleOpen} className="sm:text-xl  text-base font-semibold px-2 py-2 sm:px-4 sm:py-2 bg-purple-600 flex items-center justify-center gap-2 rounded-xl text-white" ><FaPlus/>Add Content</button>
-            </div>
+                <button className="sm:text-xl  text-base font-semibold px-2 py-2 sm:px-4 sm:py-2 bg-purple-300 flex items-center justify-center gap-2 rounded-xl text-purple-500" ><CiShare2/>Share Brain</button>
+                 <div className="flex justify-center items-center gap-2"><span className=" rounded-full bg-purple-400 text-white uppercase text-xl w-8  h-8 font-bold  inline-flex justify-center items-center">{isUser.charAt(0)}</span><span className="text-xl font-bold capitalize tracking-tighter">{isUser}</span></div>
+                <button onClick={HandleLogout} className="sm:text-lg  text-base font-semibold px-2 py-2 sm:px-3 sm:py-2 bg-red-500 flex items-center justify-center gap-2 rounded-xl text-white" >Log out<IoMdExit /></button>
+            </div>: <div className="flex gap-4">
+                    <button onClick={()=>navigate('/login')} className="px-3 py-2 bg-purple-400 text-white font-semibold text-xl rounded-xl ">Log in </button>
+                    <button onClick={()=>navigate('/register')} className="px-3 py-2 bg-purple-400 text-white font-semibold text-xl rounded-xl ">Register</button>
+                </div>}
             {isOpen && <AddContent setIsOpen = {setIsOpen} />}
         </div>
         
